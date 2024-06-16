c9a3b4689dc17c4289ce017f6a2bf3454763131c
yogyy <yogimr261@gmail.com>
2024-06-16 21:49:18 +0700

    chore: update routes

--- start/routes.ts
+++ start/routes.ts
@@ -11,6 +11,10 @@ import router from '@adonisjs/core/services/router'
 const TransactionsController = () => import('#controllers/transactions_controller')
 const ProductsController = () => import('#controllers/products_controller')
 
-router.post('/', [TransactionsController, 'createTransaction'])
 router.get('/products', [ProductsController, 'getProduct'])
-router.post('/product', [ProductsController, 'createProduct'])
+router.post('/products', [ProductsController, 'createProduct'])
+
+router.post('/transactions', [TransactionsController, 'createTransaction'])
+router.get('/transactions', [TransactionsController, 'getTransactions'])
+router.get('/transactions/:transaction_id', [TransactionsController, 'getTransactionById'])
+router.put('/transactions/:transaction_id', [TransactionsController, 'updateTransactionStatus'])
